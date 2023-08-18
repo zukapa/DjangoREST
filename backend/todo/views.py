879322dc1753905e88eacfa405b7931beaef215f
zukapa <kmvsec@gmail.com>
2023-08-18 16:45:54 +0300

    Temp commit

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,13 +1,30 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, UpdateModelMixin
 from todo.models import Project, ToDo
 from .serializers import ProjectSerializer, ToDoSerializer
+from .filters import ProjectFilter
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 100
 
 
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class ToDoViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        
+
