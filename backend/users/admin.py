96b2549bd8a383b7782ac16199468a98a4ae9ff0
zukapa <kmvsec@gmail.com>
2023-09-24 16:48:47 +0300

    Temporary changed

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,18 +4,4 @@ from users.models import User
 # Register your models here.
 
 
-class CustomUserAdmin(UserAdmin):
-    def get_form(self, request, obj=None, **kwargs):
-        if obj.groups.filter(name='Admins').exists():
-            obj.is_staff = True
-        else:
-            obj.is_staff = False
-
-        defaults = {}
-        if obj is None:
-            defaults["form"] = self.add_form
-        defaults.update(kwargs)
-        return super().get_form(request, obj, **defaults)
-
-
 admin.site.register(User, UserAdmin)
