2208d4d59ae4b82d70bc6399977f81ea1a70cd5f
zukapa <kmvsec@gmail.com>
2023-09-24 16:47:44 +0300

    Temporary changed

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,8 +3,19 @@ from django.contrib.auth.admin import UserAdmin
 from users.models import User
 # Register your models here.
 
-# class CustomUserAdmin(UserAdmin):
+
+class CustomUserAdmin(UserAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        if obj.groups.filter(name='Admins').exists():
+            obj.is_staff = True
+        else:
+            obj.is_staff = False
+
+        defaults = {}
+        if obj is None:
+            defaults["form"] = self.add_form
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
 
 
 admin.site.register(User, UserAdmin)
-
--- backend/users/models.py
+++ backend/users/models.py
@@ -4,3 +4,21 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+
+    def groups_changed(**kwargs):
+        action = kwargs.pop('action', None)
+        instance = kwargs.pop('instance', None)
+        groups = instance.groups.get_queryset()
+        if action == 'post_add':
+            for group in groups:
+                if group.name == 'Admins':
+                    instance.is_staff = True
+                instance.save()
+        if action == 'post_remove':
+            for group in groups:
+                if group.name != 'Admins':
+                    instance.is_staff = False
+                instance.save()
+
+
+models.signals.m2m_changed.connect(User.groups_changed, sender=User.groups.through)
