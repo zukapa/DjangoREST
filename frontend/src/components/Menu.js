818f46dcc4a6f40cf006d56df7d662f0b95d8df5
zukapa <kmvsec@gmail.com>
2023-09-28 15:02:43 +0300

    Seventh lesson completed.

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -2,9 +2,10 @@ import React from 'react'
 import {BrowserRouter, Route, Link, Routes} from 'react-router-dom'
 import Projects from './Projects.js'
 import ToDoList from './Todolist.js'
+import AuthForm from './Auth.js'
 import Users from './Users.js'
 
-const Menu = ({todolist, projects, users}) => {
+const Menu = ({todolist, projects, users, getToken, logOut, isAuth}) => {
     return (
         <BrowserRouter>
             <nav>
@@ -18,12 +19,17 @@ const Menu = ({todolist, projects, users}) => {
                     <li>
                         <Link to='/users'>Users</Link>
                     </li>
+                    <li>
+                        {isAuth() ? <button onClick={() => logOut()}>Logout</button> : <Link to='/login'>Login</Link>}
+                    </li>
                 </ul>
             </nav>
             <Routes>
-                <Route exact path='/' element = {<ToDoList todolist={todolist}/>}/>
-                <Route exact path='/projects' element = {<Projects projects={projects}/>}/>
-                <Route exact path='/users' element = {<Users users={users}/>}/>
+                <Route exact path='/' element = {<ToDoList todolist={todolist} />} />
+                <Route exact path='/projects' element = {<Projects projects={projects} />} />
+                <Route exact path='/users' element = {<Users users={users} />} />
+                <Route exact path='/login' element = {<AuthForm getToken={(username, password) => {getToken(username,
+                    password)}} />} />
             </Routes>
         </BrowserRouter>
     )
